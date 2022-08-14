ad34d2cbd7cc4f63a87ecb784a0afa14711e4f3b
Ryan McVicker <tvheadmail@protonmail.com>
2022-08-13 23:31:06 -0400

    added trie.c

---
+++ searching/trie.c
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h> 
+
+//Trie data structure ( Prefix tree in C ) , Ryan McVicker ( ryanmcvicker@protonmail.com ) 
+
+
+// problem : 
+// Assume you have a dictionary of letters, find all letters in the dictionary that are made up 
+// of N randomly selected letters 
+
+//also i'm trying to learn about structures 
+
+#define N 26
+//[define statement ] [data type] [name] [pointer name ]?
+//typedef struct Node node;
+
+//points on a coordinate plane 
+struct point { 
+  int x; // int x member 
+  int y; // int y member 
+};
+
+int main() {
+        
+        
+        return 0;
+}
